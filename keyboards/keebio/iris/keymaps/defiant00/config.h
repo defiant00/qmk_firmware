b68cd8f62cb535355f31979c915fa4f8c19f647d
defiant00 <defiant00@gmail.com>
2022-01-31 13:25:12 -0800

    permissive hold and shift on z, /

--- keyboards/keebio/iris/keymaps/defiant00/config.h
+++ keyboards/keebio/iris/keymaps/defiant00/config.h
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define EE_HANDS
 
-#define HOLD_ON_OTHER_KEY_PRESS
+#define PERMISSIVE_HOLD
 
 #define SPLIT_LAYER_STATE_ENABLE
 
