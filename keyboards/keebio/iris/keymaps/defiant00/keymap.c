514f861fe7df9a721b4b52fd08aa3350ca0a10f5
defiant00 <defiant00@gmail.com>
2022-03-24 10:47:51 -0700

    removed ralt on quote hold

--- keyboards/keebio/iris/keymaps/defiant00/keymap.c
+++ keyboards/keebio/iris/keymaps/defiant00/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT(
     KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                                   KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_BSPC,
     KC_LGUI,    KC_Q,       KC_W,       KC_F,       KC_P,       KC_B,                                   KC_J,       KC_L,       KC_U,       KC_Y,       KC_SCLN,    KC_DEL,
-    KC_LC_TAB,  KC_A,       KC_R,       KC_S,       KC_T,       KC_G,                                   KC_M,       KC_N,       KC_E,       KC_I,       KC_O,       KC_RA_QUOT,
+    KC_LC_TAB,  KC_A,       KC_R,       KC_S,       KC_T,       KC_G,                                   KC_M,       KC_N,       KC_E,       KC_I,       KC_O,       KC_QUOT,
     KC_LS_CAPS, KC_LS_Z,    KC_X,       KC_C,       KC_D,       KC_V,       KC_HOME,        KC_END,     KC_K,       KC_H,       KC_COMM,    KC_DOT,     KC_RS_SLSH, KC_RS_ENT,
                                                     KC_LCTL,    KC_BS_L1,   KC_DEL,         KC_SPC,     KC_ENT_L2,  KC_LALT
 ),
